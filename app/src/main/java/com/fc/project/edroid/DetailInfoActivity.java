f5200b8e7c7948323960e38d3429c4e15dc90ffa
Ravi <immaturegroup@gmail.com>
2018-03-11 23:39:56 +0530

    DetailActvity added with minor ui upgrades

---
+++ app/src/main/java/com/fc/project/edroid/DetailInfoActivity.java
@@ -0,0 +1,13 @@
+package com.fc.project.edroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+    }
+}
