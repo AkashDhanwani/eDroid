6485334f282c6827659791111690e10c86b9176d
Ravi <immaturegroup@gmail.com>
2018-03-16 23:52:49 +0530

    Disclaimer Actvity added| layouts of horizontal recyclerView added| Minor ui updates

---
+++ app/src/main/java/com/fc/project/edroid/Disclaimer.java
@@ -0,0 +1,19 @@
+package com.fc.project.edroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Disclaimer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disclaimer);
+    }
+
+    public void amazon(View view) {
+      //TODO intent to amazon website/app
+    }
+}
